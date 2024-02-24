721d0c38c600cee7bce2f3c42c05e82d584963ab
KrasnovYuri <krasnov1yuri@gmail.com>
2024-02-24 18:34:07 +0300

    Making changes permanent with UserDefaults

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -12,6 +12,8 @@ struct AddView: View {
     @State private var type = "Personal"
     @State private var amount = 0.0
     
+    @Environment(\.dismiss) var dismiss
+    
     var expenses: Expenses
     
     let types = ["Business", "Personal"]
@@ -30,6 +32,13 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
         }
     }
 }
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,8 +6,8 @@
 //
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
-    let id = UUID()
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -15,7 +15,24 @@ struct ExpenseItem: Identifiable {
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoder = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoder, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items"){
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
 }
 
 struct ContentView: View {
